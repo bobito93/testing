7896cf8503d6eae5afb83275c4c4b1b4b75136c7
alex <alexito932002@icloud.com>
2020-10-11 17:59:56 +0200

    One more

--- febrer.php
+++ febrer.php
@@ -2,3 +2,4 @@ echo 'pel febrer carnestoltes el primer';
 echo 'pel febrer flor a l'atmeller';
 echo 'Mes tard o mes matiner carnaval dins del febrer';
 echo 'Febrer,Febreret,set capes i un barret';
+echo 'Has de llaurar al Febrer el que al marc vulgues sembrar';
