7aeac16bdef264c8031c91aaf416d84f2463f2de
alex <alexito932002@icloud.com>
2020-10-11 17:52:02 +0200

    Revert "New line febrer"
    This reverts commit da0e30f9bacfa3d15ea066a5da9a5568220f7c0d.

--- febrer.php
+++ febrer.php
@@ -1,4 +1,3 @@
 echo 'pel febrer carnestoltes el primer';
 echo 'pel febrer flor a l'atmeller';
 echo 'Mes tard o mes matiner carnaval dins del febrer';
-echo 'Febrer,Febreret,set capes i un barret';
